3b013ac6dd948a012755a3ead02f184b8fe28209
bhavyaLalchandani <bhavyal321@gmail.com>
2024-12-03 11:24:13 +0530

    UC 1 Welcome Added

---
+++ src/LineComparison.java
@@ -0,0 +1,6 @@
+public class LineComparison {
+    public static void main(String[] args) {
+        // USE CASE 1 - Welcome Message
+        System.out.println("Welcome to the Line Comparison Problem");
+    }
+}
